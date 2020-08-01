10bbd9550b939aa1a0b1ca598aeb39abfb76dcab
YanaPIIDXer <yana_p_iidxer@yahoo.co.jp>
2020-08-01 21:49:12 +0900

    Wordpressをインストールするようにした。

--- setup_server.sh
+++ setup_server.sh
@@ -2,8 +2,18 @@
 
 sudo yum update -y
 
+# install apache.
 sudo yum install -y httpd
+sudo amazon-linux-extras install -y php7.2
+sudo systemctl enable httpd
+
+# install mysql client.
 sudo yum install -y mysql
 
-sudo systemctl enable httpd
+# install Wordpress and deploy to apache directory.
+wget https://ja.wordpress.org/latest-ja.tar.gz
+tar xzvf latest-ja.tar.gz
+cd wordpress
+sudo cp -r * /var/www/html/
+sudo chown apache:apache /var/www/html/ -R
 sudo systemctl start httpd
