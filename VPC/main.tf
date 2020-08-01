9b78b59fb38db0d605c51ad290be02c2db5eb236
YanaPIIDXer <yana_p_iidxer@yahoo.co.jp>
2020-08-01 13:12:40 +0900

    複数のサブネットが生成される時はNameに連番を付けるようにした。

--- VPC/main.tf
+++ VPC/main.tf
@@ -11,7 +11,7 @@ resource "aws_subnet" "public_subnets" {
   vpc_id     = aws_vpc.default.id
   cidr_block = element(var.public_subnets, count.index)
   tags = {
-    Name = join("", list("${var.name}", "_public_", "${count.index}"))
+    Name = join("", list("${var.name}", "_public"), (length(var.public_subnets) > 1) ? list("_", "${count.index + 1}") : list(""))
   }
 }
 
@@ -20,7 +20,7 @@ resource "aws_subnet" "private_subnets" {
   vpc_id     = aws_vpc.default.id
   cidr_block = element(var.private_subnets, count.index)
   tags = {
-    Name = join("", list("${var.name}", "_private_", "${count.index}"))
+    Name = join("", list("${var.name}", "_private"), (length(var.private_subnets) > 1) ? list("_", "${count.index + 1}") : list(""))
   }
 }
 
