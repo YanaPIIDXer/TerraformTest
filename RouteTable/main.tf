f670c8ec94d2c818947b5a45bb3a967ee7a90d23
YanaPIIDXer <yana_p_iidxer@yahoo.co.jp>
2020-08-01 13:54:29 +0900

    SubnetAssociationを復帰。

--- RouteTable/main.tf
+++ RouteTable/main.tf
@@ -5,3 +5,9 @@ resource "aws_route_table" "default" {
       Name = var.name
   }
 }
+
+resource "aws_route_table_association" "subnet_association" {
+    count = length(var.subnets)
+    subnet_id = var.subnets[count.index].id
+    route_table_id = aws_route_table.default.id
+}
